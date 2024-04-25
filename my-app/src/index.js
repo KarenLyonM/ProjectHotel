6fadc6b0480c696ca55c1a7c21fafd21320b4d75
KarenLyonM <karensofialyon@gmail.com>
2024-04-25 11:02:33 +0100

    tailwind and NavBar

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./../src/App.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar>
+
+      <App />
+    </NavBar>
   </React.StrictMode>
 );
 
